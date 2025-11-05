ac0e069db07a69ba60aa5c3d6bc45a4f14fe90fc
yTe4Ka <82293367+Ytka-c-cOdOiNom@users.noreply.github.com>
2025-11-05 16:36:48 +0200

    Add files via upload

---
+++ database/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS items (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO items (name)
+SELECT 'Item 1 (from Database)'
+WHERE NOT EXISTS (SELECT 1 FROM items WHERE name = 'Item 1 (from Database)');
+
+INSERT INTO items (name)
+SELECT 'Item 2 (from Database)'
+WHERE NOT EXISTS (SELECT 1 FROM items WHERE name = 'Item 2 (from Database)');
